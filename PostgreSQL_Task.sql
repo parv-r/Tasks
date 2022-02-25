f66fd7edae4887ad2101ef652a61b000cc31c938
parv-r <parv.rastogi@innovaccer.com>
2022-02-25 16:05:50 +0530

    Fourth Commit By Parv

--- PostgreSQL_Task.sql
+++ PostgreSQL_Task.sql
@@ -1,3 +1,7 @@
+-- Database: E-Commerce
+
+-- DROP DATABASE IF EXISTS "E-Commerce";
+
 create table category(
 	cid int PRIMARY KEY,
 	cname varchar(255)
@@ -24,16 +28,24 @@ create table prod_quan_audit(
 	audit_date date
 );
 
-create table roles(
-	roleId varchar(50) PRIMARY KEY,
-	rolename varchar(100)
-);
-
 create table employees(
 	empId varchar(50) PRIMARY KEY,
 	emp_name varchar(100),
-	date_of_joining date,
-	roleId varchar(50) REFERENCES roles(roleId)
+	date_of_joining date
+);
+
+create table roles(
+	roleId varchar(50) PRIMARY KEY,
+	rolename varchar(100),
+	empId varchar(50) REFERENCES employees(empId)
+);
+
+create table emp_role_audit(
+	erid SERIAL PRIMARY KEY,
+	empId varchar(50) NOT NULL,
+	emp_name varchar(100),
+	rolename varchar(100) NOT NULL,
+	date_modified date
 );
 
 INSERT INTO category (cid, cname) VALUES
@@ -59,20 +71,19 @@ INSERT INTO orders (orderId, purc_amt, order_date, pid) VALUES
 ('692', 150000, '2022-02-09', 'E51'),
 ('719', 43000, '2022-01-07', 'A41');
 
-INSERT INTO roles (roleId, rolename) VALUES
-('1037', 'Super Admin'),
-('2048', 'Admin'),
-('3056', 'Manager'),
-('4096', 'Developer'),
-('6020', 'Tester');
+INSERT INTO employees (empId, emp_name, date_of_joining) VALUES
+('IN05', 'Aditya', '2019-03-26'),
+('IN11', 'Devansh', '2018-09-24'),
+('IN18', 'Harsh', '2020-04-16'),
+('IN21', 'Prakhar', '2020-06-10'),
+('IN45', 'Akansha', '2021-05-07');
 
-INSERT INTO employees (empId, emp_name, date_of_joining, roleId) VALUES
-('IN01', 'Abhishek', '2019-01-21', '2048'),
-('IN05', 'Aditya', '2019-03-26', '4096'),
-('IN11', 'Devansh', '2018-09-24', '1037'),
-('IN18', 'Harsh', '2020-04-16', '3056'),
-('IN21', 'Prakhar', '2020-06-10', '6020'),
-('IN45', 'Akansha', '2021-05-07', '2048');
+INSERT INTO roles (roleId, rolename, empId) VALUES
+('1037', 'Super Admin', 'IN05'),
+('2048', 'Admin', 'IN21'),
+('3056', 'Manager', 'IN11'),
+('4096', 'Developer', 'IN18'),
+('6020', 'Tester', 'IN45');
 
 create or replace view orderinfo as
 select p.pid, p.pname, o.orderId, o.purc_amt, o.order_date
@@ -83,8 +94,8 @@ select * from orderinfo;
 
 create or replace view empinfo as
 select e.*, r.rolename
-from roles r inner join employees e
-on r.roleId = e.roleId;
+from employees e inner join roles r
+on e.empId = r.empId;
 
 select * from empinfo;
 
@@ -95,15 +106,38 @@ as $$
 begin
 if new.order_date <> old.order_date then
 insert into prod_quan_audit(order_date, audit_date) values (old.order_date,now());
-update product set quantity = quantity - 1 where pid = new.orderId;
+update product set quantity = quantity - 1 where pid = new.pid;
 end if;
 return new;
 end;
 $$
 
-
 create trigger trigger_quan_chck
-after insert
+before update
 on orders
 for each row 
-execute procedure fn_quant_chck(); 
+execute procedure fn_quant_chck();
+
+update orders set order_date = '2022-02-22' where pid = 'E11';
+
+create or replace function fn_role_chng()
+returns trigger
+language plpgsql
+as $$
+begin
+if new.rolename <> old.rolename then
+insert into emp_role_audit(empId, emp_name, rolename, date_modified) values
+(old.empId, old.emp_name, new.rolename, now());
+update roles set rolename = new.rolename where roles.roleId = employees.roleId;
+end if;
+return new;
+end;
+$$
+
+create trigger trigger_role_updt
+before update
+on employees
+for each row 
+execute procedure fn_role_chng(); 
+
+update roles set rolename = 'Admin' where empId = 'IN21';
